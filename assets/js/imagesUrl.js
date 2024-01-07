98bbcc82b7f2ecd8596fcf2d6fb97b5a91f4b1e8
banasaron <aron.banas@wp.pl>
2024-01-07 18:00:18 +0100

    debugging src 4

--- assets/js/imagesUrl.js
+++ assets/js/imagesUrl.js
@@ -1,5 +1,5 @@
 const imagePath = '../images/lock-screen-wallpaper.png'
-const lockScreenUrl = window.location.origin + '/assets/images/lock-screen-wallpaper.png';
+const lockScreenUrl = window.location.origin + '/ios12/assets/images/lock-screen-wallpaper.png';
 
-console.log(imagePath);
+console.log(imagePath); 
 console.log(lockScreenUrl);
